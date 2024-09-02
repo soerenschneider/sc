f256c2f44ec675610a8316681ca7db15fef1787c
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2024-09-02 21:03:40 +0200

    feat: check for updated releases on start

--- cmd/root.go
+++ cmd/root.go
@@ -1,27 +1,57 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"strings"
+	"time"
 
+	"github.com/hashicorp/go-retryablehttp"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"github.com/soerenschneider/sc/internal"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"golang.org/x/term"
 )
 
-const rootCmdFlagsVerbose = "verbose"
+const (
+	rootCmdFlagsVerbose     = "verbose"
+	rootCmdFlagsNoTelemetry = "no-telemetry"
+)
 
 var rootCmd = &cobra.Command{
 	Use:   "sc",
 	Short: "Universal Command Line Interface for soeren.cloud",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		verbose, _ := cmd.Flags().GetBool(rootCmdFlagsVerbose)
+		setupLogLevel(verbose)
+
+		go func() {
+			disableTelemetry, err := cmd.Flags().GetBool(rootCmdFlagsNoTelemetry)
+			DieOnErr(err, "could not get flag")
+
+			if disableTelemetry || !strings.HasPrefix(internal.BuildVersion, "v") {
+				log.Debug().Str("local_version", internal.BuildVersion).Msg("not performing check for update")
+				return
+			}
+
+			httpClient := retryablehttp.NewClient().StandardClient()
+			releaseNotifier, err := internal.NewReleaseNotifier(httpClient, internal.BuildVersion)
+			if err != nil {
+				log.Warn().Msg("could not build release notifier")
+				return
+			}
+
+			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+			defer cancel()
+			releaseNotifier.CheckRelease(ctx)
+		}()
+	},
 }
 
 func Execute() {
-	verbose, _ := rootCmd.Flags().GetBool(rootCmdFlagsVerbose)
-	setupLogLevel(verbose)
-
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
@@ -32,6 +62,7 @@ func init() {
 	cobra.OnInitialize(initConfig)
 
 	rootCmd.PersistentFlags().BoolP(rootCmdFlagsVerbose, "v", false, "Print debug logs")
+	rootCmd.PersistentFlags().Bool(rootCmdFlagsNoTelemetry, false, "Do not perform check for updated version")
 }
 
 func initConfig() {
---
+++ internal/release_notifier.go
@@ -0,0 +1,87 @@
+package internal
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/rs/zerolog/log"
+)
+
+const (
+	componentName = "release_watcher"
+	owner         = "soerenschneider"
+	repo          = "sc"
+)
+
+type Client interface {
+	Do(r *http.Request) (*http.Response, error)
+}
+
+type ReleaseNotifier struct {
+	client     Client
+	tag        string
+	updateSeen bool
+}
+
+type GitHubRelease struct {
+	TagName string `json:"tag_name"`
+}
+
+func NewReleaseNotifier(client Client, tag string) (*ReleaseNotifier, error) {
+	if client == nil {
+		return nil, errors.New("nil client passed")
+	}
+	if len(tag) == 0 {
+		return nil, errors.New("empty release tag passed")
+	}
+	return &ReleaseNotifier{
+		client: client,
+		tag:    tag,
+	}, nil
+}
+
+func (r *ReleaseNotifier) CheckRelease(ctx context.Context) {
+	release, err := GetLatestRelease(ctx, r.client, owner, repo)
+	if err != nil {
+		log.Error().Str("component", componentName).Err(err).Msg("check for latest release failed")
+		return
+	}
+
+	if release != r.tag {
+		if !r.updateSeen {
+			log.Info().Str("component", componentName).Str("remote_version", release).Str("local_version", r.tag).Msg("noticed update")
+		}
+		r.updateSeen = true
+	} else {
+		log.Info().Str("component", componentName).Str("remote_version", release).Str("local_version", r.tag).Msg("no update available")
+	}
+}
+
+// GetLatestRelease fetches the latest release tag from a GitHub repository
+func GetLatestRelease(ctx context.Context, client Client, owner, repo string) (string, error) {
+	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/releases/latest", owner, repo)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("failed to fetch latest release: %s", resp.Status)
+	}
+
+	var release GitHubRelease
+	if err := json.NewDecoder(resp.Body).Decode(&release); err != nil {
+		return "", err
+	}
+
+	return release.TagName, nil
+}
