909b4b4235a226953cb388bc3a7300cafc5f518b
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2024-09-03 18:56:20 +0200

    feat: better logging

--- cmd/pkiIssue.go
+++ cmd/pkiIssue.go
@@ -2,11 +2,14 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 	"time"
 
+	"github.com/rs/zerolog/log"
 	"github.com/soerenschneider/sc/internal/pki/builder"
 	"github.com/soerenschneider/vault-pki-cli/pkg"
 	"github.com/soerenschneider/vault-pki-cli/pkg/pki"
+	"github.com/soerenschneider/vault-pki-cli/pkg/renew_strategy"
 	"github.com/soerenschneider/vault-pki-cli/pkg/storage/backend"
 	"github.com/soerenschneider/vault-pki-cli/pkg/storage/shape"
 	"github.com/spf13/cobra"
@@ -94,8 +97,25 @@ var pkiIssueCmd = &cobra.Command{
 		ctx, cancel := context.WithTimeout(context.Background(), 45*time.Second)
 		defer cancel()
 
-		_, _ = pkiService.Issue(ctx, storageImpl, issueArgs)
+		result, err := pkiService.Issue(ctx, storageImpl, issueArgs)
+		handlePkiIssueResult(result)
+		if err != nil {
+			log.Err(err).Msg("could not issue certificate")
+		}
 	},
+}
+
+func handlePkiIssueResult(result pkg.IssueResult) {
+	if result.Status == pkg.Issued {
+		if result.ExistingCert != nil {
+			percentage := fmt.Sprintf("%.1f", renew_strategy.GetPercentage(*result.ExistingCert))
+			log.Info().Msgf("Existing certificate at %s%% expired or below threshold, valid from %v until %v", percentage, result.ExistingCert.NotBefore.Format(time.RFC3339), result.ExistingCert.NotAfter.Format(time.RFC3339))
+		}
+		log.Info().Msgf("New certificate valid until %v (%s)", result.IssuedCert.NotAfter.Format(time.RFC3339), time.Until(result.IssuedCert.NotAfter).Round(time.Second))
+	} else if result.Status == pkg.Noop {
+		percentage := fmt.Sprintf("%.1f", renew_strategy.GetPercentage(*result.ExistingCert))
+		log.Info().Msgf("Existing certificate at %s%%, valid until %v (%s)", percentage, result.ExistingCert.NotAfter.Format(time.RFC3339), time.Until(result.ExistingCert.NotAfter).Round(time.Second))
+	}
 }
 
 func init() {
