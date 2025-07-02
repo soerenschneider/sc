2a78343d3f9d1340a256f34e2e9d1d3422cba816
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2025-07-02 19:27:02 +0200

    feat: improve output

--- cmd/vaultTokenLookup.go
+++ cmd/vaultTokenLookup.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/rs/zerolog/log"
 	"github.com/soerenschneider/sc/internal/tui"
@@ -11,8 +13,9 @@ import (
 
 // vaultLoginCmd represents the vaultLogin command
 var vaultTokenLookupCmd = &cobra.Command{
-	Use:   "lookup",
-	Short: "Lookup and display information about the current Vault token",
+	Use:     "lookup",
+	Aliases: []string{"ls"},
+	Short:   "Lookup and display information about the current Vault token",
 	Long: `Retrieve and display information about the currently active Vault token.
 
 This command queries the Vault API to show metadata about the current token,
@@ -31,11 +34,60 @@ It attempts to authenticate using the following sources (in order):
 
 		secret, err := client.LookupToken(ctx)
 		if err != nil {
-			log.Fatal().Msgf("could not lookup: %v", err)
+			log.Fatal().Err(err).Msg("could not lookup")
 		}
 
-		tui.PrintMapOutput(secret.Data)
+		// don't leak token
+		delete(secret.Data, "id")
+
+		// re-format times
+		var problematicColumns []string
+		expiry, ok := secret.Data["expire_time"]
+		if ok {
+			updatedValue, parsedTime := reformatTimestamp(expiry.(string))
+			if parsedTime != nil && time.Now().After(*parsedTime) {
+				problematicColumns = append(problematicColumns, "expire_time")
+			}
+			secret.Data["expire_time"] = updatedValue
+		}
+
+		issue, ok := secret.Data["issue_time"]
+		if ok {
+			updatedValue, _ := reformatTimestamp(issue.(string))
+			secret.Data["issue_time"] = updatedValue
+		}
+
+		tui.PrintMapOutput(secret.Data, problematicColumns...)
 	},
+}
+
+func reformatTimestamp(timestamp string) (string, *time.Time) {
+	parsedTime, err := time.Parse(time.RFC3339Nano, timestamp)
+	if err != nil {
+		return timestamp, nil
+	}
+
+	format := "15:04:05 MST"
+	if !isToday(parsedTime) {
+		format = "06-01-02T15:04 MST"
+	}
+
+	var relative string
+	if parsedTime.Before(time.Now()) {
+		relative = fmt.Sprintf("%s ago", time.Since(parsedTime).Round(time.Minute))
+	} else {
+		relative = fmt.Sprintf("in %s", time.Until(parsedTime).Round(time.Minute))
+	}
+
+	return fmt.Sprintf("%s (%s)", relative, parsedTime.Format(format)), &parsedTime
+}
+
+func isToday(t time.Time) bool {
+	now := time.Now().UTC()
+
+	return t.Year() == now.Year() &&
+		t.Month() == now.Month() &&
+		t.Day() == now.Day()
 }
 
 func init() {
