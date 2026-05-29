4929e9d0abd09aba9459437c5e1435dcd65336c3
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2026-05-29 21:24:41 +0100

    refactor: change signature

--- internal/vault/formatter/json.go
+++ internal/vault/formatter/json.go
@@ -7,7 +7,7 @@ import (
 type JsonFormatter struct {
 }
 
-func (y *JsonFormatter) Format(data map[string]any) ([]byte, error) {
+func (y *JsonFormatter) Format(data map[string]string) ([]byte, error) {
 	marshalled, err := json.Marshal(&data)
 	if err != nil {
 		return nil, err
--- internal/vault/formatter/yaml.go
+++ internal/vault/formatter/yaml.go
@@ -5,7 +5,7 @@ import "gopkg.in/yaml.v3"
 type YamlFormatter struct {
 }
 
-func (y *YamlFormatter) Format(data map[string]any) ([]byte, error) {
+func (y *YamlFormatter) Format(data map[string]string) ([]byte, error) {
 	marshalled, err := yaml.Marshal(&data)
 	if err != nil {
 		return nil, err
